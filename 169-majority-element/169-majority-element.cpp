6d39e139dd12d9f0c6eed33e0e4a5e2321ea3809
Satwikan <69719072+Satwikan@users.noreply.github.com>
2022-01-20 12:21:49 +0530

    Time: 36 ms (17.62%), Space: 19.7 MB (8.61%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        if (nums.size() == 1) return nums[0];
         sort(nums.begin(), nums.end());
-        // cout << nums[0] << nums[1] << nums[2];
         return nums[(nums.size()-1)/2];
     }
 };
