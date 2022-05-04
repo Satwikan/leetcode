9444d41c70dc24eab2ada3637cd18e96684f169e
Satwikan <69719072+Satwikan@users.noreply.github.com>
2022-05-03 20:56:40 -0700

    Time: 180 ms (60.74%), Space: 67.6 MB (48.57%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        
-        int i = 0, j = nums.size() - 1;
-        int count = 0;
-        while (i < j) {
-            if (nums[i] + nums[j] < k) i++;
-            else if (nums[i] + nums[j] > k) j--;
-            else if (nums[i] + nums[j] == k) {
-                // cout << i << " " << j << endl;
-                // cout << nums[i] << " " << nums[j] << endl;
-                j--;
-                i++;
-                count++;
-            }
+      unordered_map<int, int> Map;
+      for (auto &num: nums) Map[num]++;  // count freq of nums
+      int ans = 0;
+  
+      for(auto it=Map.begin(); it!=Map.end(); ++it){
+        int num = it->first, count = it->second;
+        if(k - num == num) ans += count/2;   // if num is half of k add half of it's count in ans
+        else if(Map.count(k - num)){   // find k-num in nums and add min freq of num or k-num to ans
+          int Min = min(count, Map[k-num]);
+          ans += Min;
+          Map[num] -= Min;
+          Map[k-num] -= Min;
         }
-        return count;
+      }
+      
+      return ans;
     }
 };
