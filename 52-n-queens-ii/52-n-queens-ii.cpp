c48b8174fbc1df2448b983ab4140c2474eb501a4
Satwikan <69719072+Satwikan@users.noreply.github.com>
2022-06-05 09:27:03 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (97.40%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,53 +1,13 @@
 class Solution {
 public:
-    bool check(vector<string>& board, int i, int j) {
-        int n = board.size();
-        for (int k = 0; k < n; k++) {
-            if (board[i][k] == 'Q' || board[k][j] == 'Q') return false;
-        }
-        // diagonal check
-        int it = i-1, jt = j-1;
-        while (it >= 0 && jt >= 0) {
-            if (board[it][jt] == 'Q') return false;
-            it--;jt--;
-        }
-        it = i+1; jt = j+1;
-        while (it < n && jt < n) {
-            if (board[it][jt] == 'Q') return false;
-            it++;jt++;
-        }
-        it = i-1; jt = j+1;
-        while (it >= 0 && jt < n) {
-            if (board[it][jt] == 'Q') return false;
-            it--;jt++;
-        }
-        it = i+1; jt = j-1;
-        while (it < n && jt >= 0) {
-            if (board[it][jt] == 'Q') return false;
-            it++;jt--;
-        }
-        return true;
+    int totalNQueens(int n, int row = 0, int col = 0, int diag = 0, int anti_diag = 0) {
+    if(row == n) return 1;
+    int count = 0;
+	for(int column = 0; column < n; column++) {
+        int col_mask = 1 << column, diag_mask = 1 << row + column, anti_diag_mask = 1 << row - column + n - 1;
+        if(!(col & col_mask) && !(diag & diag_mask) && !(anti_diag & anti_diag_mask))
+			count += totalNQueens(n, row + 1, col | col_mask, diag | diag_mask, anti_diag | anti_diag_mask);
     }
-    void backtrack(int n, int col, vector<string>& board, vector<vector<string>>& res) {
-        if (col == n) {
-            res.push_back(board);
-            return;
-        }
-        for (int i = 0; i < n; i++) {
-            if (check(board, i, col)) {
-                board[i][col] = 'Q';
-                backtrack(n, col+1, board, res);
-                board[i][col] = '.';
-            }
-        }
-        
-    }
-    int totalNQueens(int n) {
-        string strq = "";
-        for (int i = 0; i < n; i++) strq += '.';
-        vector<string> board(n, strq);
-        vector<vector<string>> res;
-        backtrack(n, 0, board, res);
-        return res.size();
-    }
+	return count;
+}
 };
