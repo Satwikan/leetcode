3c67fdf0fc659b29d753dec89975c518040d2627
Satwikan <69719072+Satwikan@users.noreply.github.com>
2022-06-06 09:23:14 +0530

    Time: 49 ms (84.74%), Space: 14.5 MB (88.39%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -8,28 +8,33 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode * curr = headA;
-        int l1 = 0;
-        while(curr != NULL) {
-            curr = curr->next;
-            l1++;
-        }
-        curr = headB;
-        int l2 = 0;
-        while(curr != NULL) {
-            curr = curr->next;
-            l2++;
-        }
-        while (headA != headB) {
-            if (l1 > l2) {
-                headA = headA->next;
-                l1--;
-            } else {
-                headB = headB->next;
-                l2--;
-            }
-        }
-        return headA;
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) 
+{
+    ListNode *p1 = headA;
+    ListNode *p2 = headB;
+        
+    if (p1 == NULL || p2 == NULL) return NULL;
+
+    while (p1 != NULL && p2 != NULL && p1 != p2) {
+        p1 = p1->next;
+        p2 = p2->next;
+
+        //
+        // Any time they collide or reach end together without colliding 
+        // then return any one of the pointers.
+        //
+        if (p1 == p2) return p1;
+
+        //
+        // If one of them reaches the end earlier then reuse it 
+        // by moving it to the beginning of other list.
+        // Once both of them go through reassigning, 
+        // they will be equidistant from the collision point.
+        //
+        if (p1 == NULL) p1 = headB;
+        if (p2 == NULL) p2 = headA;
     }
+        
+    return p1;
+}
 };
