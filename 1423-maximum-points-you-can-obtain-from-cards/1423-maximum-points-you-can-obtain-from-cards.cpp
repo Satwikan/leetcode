a835e3197f991fff42c9994458866dd16fcb07f5
Satwikan <69719072+Satwikan@users.noreply.github.com>
2022-06-26 11:57:51 +0530

    Time: 79 ms (64.79%), Space: 45.7 MB (8.03%) - LeetHub

---
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int sum = 0;
+        int n = cardPoints.size();
+        
+        vector<int> cummulativeSumFromFront(n+1, 0);
+        vector<int> cummulativeSumFromBehind(n+1, 0);
+        
+        sum = 0;
+        for (int i=0; i<n; i++) {
+            sum += cardPoints[i];
+            cummulativeSumFromFront[i+1] = sum;
+        }
+        sum = 0;
+        for (int i=n-1; i>=0; i--) {
+            sum += cardPoints[i];
+            cummulativeSumFromBehind[i] = sum;
+        }
+        
+        // Reversing is optional. I reversed it so that it would be easy
+        // to access sum of last (k-i) elements by just indexing at [k-i]
+        // Otherwise, I would have had to index it at [n-k+i] which would
+        // have made it difficult to read.
+        reverse(cummulativeSumFromBehind.begin(), cummulativeSumFromBehind.end());
+        
+        int answer = 0;
+        for(int i=0; i<=k; i++) {      
+            answer = max(answer, 
+                           cummulativeSumFromFront[i] // Sum of first 'i' cards.
+                         + cummulativeSumFromBehind[k-i]); // Sum of last 'k-i' cards.
+        }
+        return answer;
+    }
+};
