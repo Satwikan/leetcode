9139ed50978c0c7336626ac3b315b96d52a7cf67
Satwikan <69719072+Satwikan@users.noreply.github.com>
2022-07-03 11:54:32 +0530

    Time: 3 ms (69.26%), Space: 7.2 MB (43.94%) - LeetHub

---
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    // Time: O(n)
+    // Space: O(1)
+    int wiggleMaxLength(vector<int>& nums) {
+        if (nums.size() < 2) return nums.size();
+        int down = 1, up = 1;
+        for(int i = 1; i < nums.size(); i++) {
+            if (nums[i] > nums[i-1]) up = down + 1;
+            else if (nums[i] < nums[i-1]) down = up + 1;
+        }
+        return max(up, down);
+    }
+};
