fe89e1df6b38c3b3be563bfe68b73f8fc95b8f04
Satwikan <69719072+Satwikan@users.noreply.github.com>
2022-07-10 09:07:21 +0530

    Time: 17 ms (18.02%), Space: 13.6 MB (79.79%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // O(n), space: O(1)
     int minCostClimbingStairs(vector<int>& cost) {
         int m1 = cost[0], m2 = cost[1];
         for (int i = 2; i < cost.size(); i++) {
