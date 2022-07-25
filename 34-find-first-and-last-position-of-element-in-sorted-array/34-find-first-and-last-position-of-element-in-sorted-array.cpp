8109a39f353af10f3d545711a480e6e0dc6f39d9
Satwikan <69719072+Satwikan@users.noreply.github.com>
2022-07-25 09:10:41 +0530

    Time: 12 ms (60.23%), Space: 13.4 MB (98.09%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        // find in between of elements in array
         int n = nums.size();
-        // if (nums.size() == 1 && nums[0] == target) return {0, 0};
         int start = 0, end = nums.size()-1;
         while (end >= start) {
             int mid = start+(end-start)/2;
