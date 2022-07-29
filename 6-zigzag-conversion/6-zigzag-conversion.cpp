568d65618a58bcdfa49e87baadad749e89a3712c
Satwikan <69719072+Satwikan@users.noreply.github.com>
2022-07-29 16:11:29 +0530

    Time: 12 ms (74.48%), Space: 8.4 MB (67.28%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.cpp
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
+    // Time Complexity: O(n)
+    // Space: O(n)
     string convert(string s, int numRows) {
         if (numRows == 1) return s;
         string a[numRows];
         int r = 0;
         bool inc = false;
         for(auto c: s) {
-            // cout << c << r << " ";
             a[r] += c;
             if (r == numRows-1 || r == 0) inc = !inc;
             r = inc ? r+1 : r-1;
         }
         string res = "";
-        for (auto c: a) {
-            // cout << c << endl;
-            res += c;
-        }
+        for (auto c: a) res += c;
         return res;
     }
 };
