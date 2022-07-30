d83d9bda0e5e8f4eeb4158b165fdd3d4ea745e65
Satwikan <69719072+Satwikan@users.noreply.github.com>
2022-07-30 18:50:54 +0530

    Time: 148 ms (27.39%), Space: 48.3 MB (78.19%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        bool memo[n+1];
+        memo[1] = true;
+        for (int i = 2; i <= n; i++) {
+            int j = i-1;
+            memo[i] = false; 
+            while (j > 0) {
+                if (memo[j] && nums[j-1] + j >= i) {
+                    memo[i] = true;
+                    break;
+                } j--;
+            }
+        }
+        return memo[n];
+    }
+};
