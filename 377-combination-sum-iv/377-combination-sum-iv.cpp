2a9d970651caca6111ff1029789e6df690b502f7
Satwikan <69719072+Satwikan@users.noreply.github.com>
2022-08-05 12:00:57 +0530

    Time: 3 ms (62.90%), Space: 6.6 MB (65.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -4,16 +4,11 @@ public:
         vector<uint> dp(target + 1);
         dp[0] = 1;
         for (int j = 1; j < target+1; j++) {
-            // cout << j << ": ";
             for (auto& num : nums){
-                // if (nums[i] == j) dp[j] += 1;
                 if (num <= j) dp[j] += dp[j-num];
                 
             }
-            // cout << j << " " << dp[j] << " ";
-            // cout << endl;
         }
-        // for (int j = 1; j < target+1; j++) cout << dp[j] << " ";
         return dp[target];
     }
 };
