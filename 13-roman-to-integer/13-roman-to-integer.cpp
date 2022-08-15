1a100d5561dccc9030cc821cedd0b02a20cbc1d1
Satwikan <69719072+Satwikan@users.noreply.github.com>
2022-08-15 11:10:24 +0530

    Time: 19 ms (50.83%), Space: 8.2 MB (30.55%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
+    // Time Complexity: O(n)
     int romanToInt(string s) {
         unordered_map<char, int> mp = 
-        {{'-1',             0},
-        {'I',             1},
-        {'V',             5},
-        {'X',             10},
-        {'L',             50},
-        {'C',             100},
-        {'D',             500},
-        {'M',             1000}};
+            {{'I',             1},
+            {'V',             5},
+            {'X',             10},
+            {'L',             50},
+            {'C',             100},
+            {'D',             500},
+            {'M',             1000}};
         char prev = s[0];
         int res = 0;
         for (int i = 1; i < s.size(); i++) {
             if (prev != '0' && mp[s[i]] > mp[prev]) {
                 res += mp[s[i]] - mp[prev];
                 prev = '0';
-                // cout << res << " if " << prev << endl;
             } else {
                 res += mp[prev];
                 prev = s[i];
-                // cout << res << " else " << prev << endl;
             }
             
         }
